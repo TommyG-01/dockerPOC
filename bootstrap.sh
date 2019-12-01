35887208ccefafcb4e207ffc1aef4e25f4e16303
Tommy G <53332879+TommyG-01@users.noreply.github.com>
2019-12-01 17:57:20 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ sudo apt install -y maven
 #mvn -version
 
 # getting the springboot applications
-git clone -b v2.1.3.RELEASE --single-branch --depth 1 https://github.com/spring-projects/spring-boot.git /tmp/spring-boot
+#git clone -b v2.1.3.RELEASE --single-branch --depth 1 https://github.com/spring-projects/spring-boot.git /tmp/spring-boot
 
 sudo mvn -f /tmp/spring-boot/spring-boot-samples$ cd spring-boot-sample-web-ui/pom.xml clean install
 
